9bab3558d8fc8df9f40aa8f313763a9770916547
Brandon <btgrant6@gmail.com>
2018-09-17 15:34:38 -0500

    Initial commit

---
+++ wheel.rb
@@ -0,0 +1,20 @@
+class Wheel
+  FACTOR = 5.0 / 9
+  attr_accessor :number
+  def func=(fac)
+    @number = (fac - 32) * FACTOR
+  end
+
+  def func2
+    (@number / FACTOR) + 32
+  end
+
+  def initialize(num)
+    @number = num
+  end
+end
+
+t = Wheel.new(25)
+puts t.func2 # 77.0
+t.func = 60 # invokes f=()
+puts t.number # 15.55
