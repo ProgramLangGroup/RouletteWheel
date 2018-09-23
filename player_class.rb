ae2e9a9380c7973261591aa857688c6abb2b1ad3
etwilley <43356808+etwilley@users.noreply.github.com>
2018-09-23 15:48:35 -0500

    Add files via upload

--- player_class.rb
+++ player_class.rb
@@ -1,42 +1,37 @@
 class Player_Class
-  playerWallet = 100
 
-  print "What is your name? \n"
+
+  print 'What is your name? \n'
   playerName = gets.chomp.capitalize
-  puts 'Welcome ' + playerName + "! \n"
+  puts 'Welcome ' + playerName + '!'
+  puts 'How much would you like to put in your bank?'
+  playerWallet = gets.to_i
+  puts 'How much money would you like to make?'
+  playerGoal = gets.to_i
 
-  while playerWallet > 0
+  while (playerWallet > 0) && (playerWallet < playerGoal)
 
-    puts "You have money for a wager. Would you like to wager? Yes/No \n"
-    wagerAsk = gets.chomp.downcase
-
-    if wagerAsk == 'yes'
-      puts "How much would you like to wager? \n"
-      playerWager = gets.to_i
-
+    puts 'How much would you like to wager?'
+    playerWager = gets.to_i
 
       if (playerWager > 0) && (playerWager <= playerWallet)
         puts 'Where would you like to place your wager? Please select either red or black'
         wagerColor = gets.downcase.chomp
 
-        if wagerColor == 'r'
+        if wagerColor == 'red'
           puts 'this is where the roulette function would be called - red'
-        elsif wagerColor == 'b'
+        elsif wagerColor == 'black'
           puts 'This is where the roulette function would be called - black'
         else
-          puts 'thats not a valid color'
+          puts 'that is not a valid color'
+        end
 
-          end
-
-       else puts 'Please place a valid bet. You only have $' + playerWallet.to_s + "\n"
+      else puts 'Please place a valid bet. You only have $' + playerWallet.to_s + "\n"
       end
-
-    elsif wagerAsk == 'no'
-      puts 'Later Tater!'
-
     end
   end
-end
+
+
 
 
       
