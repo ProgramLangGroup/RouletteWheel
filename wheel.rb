b76e587b3d571946123d0871f60c04772f4d1124
Brandon <btgrant6@gmail.com>
2018-09-23 18:27:00 -0500

    Updates

--- wheel.rb
+++ wheel.rb
@@ -1,20 +1,99 @@
 class Wheel
-  FACTOR = 5.0 / 9
-  attr_accessor :number
-  def func=(fac)
-    @number = (fac - 32) * FACTOR
+
+  def spin
+    @spin = rand(37)
+
+    if (@spin >= 1) and (@spin <= 10) and (@spin.even?)
+      @slot = 'black'
+
+    elsif (@spin >= 11) and (@spin <= 17) and (@spin.odd?)
+      @slot = 'black'
+
+    elsif (@spin >= 20) and (@spin <= 28) and (@spin.even?)
+      @slot = 'black'
+
+    elsif (@spin >= 29) and (@spin <= 35) and (@spin.odd?)
+      @slot = 'black'
+
+    else
+      @slot = 'red'
+    end
   end
 
-  def func2
-    (@number / FACTOR) + 32
+  def betting
+    if @bank > 0
+      spin
+      if @color == @slot[0,1].upcase
+        @bank = @bank + (@bet * 2)
+        puts 'You landed on ' + @slot
+        puts 'You won the bet! Your bank is now at $' + @bank.to_s
+        Prompt.prompts(@bank)
+      else
+        @bank -= @bet
+        puts 'You landed on ' + @slot
+        puts 'You lost the bet. Your bank is now at $' + @bank.to_s
+        Prompt.prompts(@bank)
+      end
+
+    elsif @bank >= @payout
+      puts 'You have reached your desire payout. The game is now over. Congratulations!'
+    else
+      'Sorry, you have nothing left in the bank. The game is now over.'
+    end
   end
 
-  def initialize(num)
-    @number = num
+  def double_up
+    @number = @number
+  end
+
+  def initialize(bank, bet, color, payout)
+    @bank = bank
+    @bet = bet
+    @color = color
+    @payout = payout
   end
 end
 
-t = Wheel.new(25)
-puts t.func2 # 77.0
-t.func = 60 # invokes f=()
-puts t.number # 15.555555555555557
+class Prompt
+
+  def self.place_bet
+    puts 'What is your bet?'
+    @bet = gets.chomp.to_i
+    check_bet
+  end
+
+  def self.check_bet
+    if @bet > @bank
+      puts 'You cannot bet more than you have in the bank.'
+      place_bet
+    end
+  end
+
+  def self.pick_color
+    puts 'Pick a color: Type "R" for red or "B" for black.'
+    @color = gets.chomp.upcase.to_s
+    check_color
+  end
+
+  def self.check_color
+    unless @color.eql? 'R' or @color.eql? 'B'
+      puts @color + ' is not a valid input.'
+      pick_color
+    end
+  end
+
+  def self.prompts(bank)
+    @bank = bank
+    place_bet
+    pick_color
+    play = Wheel.new(bank, @bet, @color, @payout)
+    play.betting
+  end
+
+  puts 'How much money do you have?'
+  @bank = gets.chomp.to_i
+  puts 'What is your desired bank total?'
+  @payout = gets.chomp.to_i
+  prompts(@bank)
+end
+
