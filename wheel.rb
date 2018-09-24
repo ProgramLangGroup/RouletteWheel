166ed70788ac613292533ddcee08449b32dc7c73
Brandon <btgrant6@gmail.com>
2018-09-23 23:17:12 -0500

    Revisions

--- wheel.rb
+++ wheel.rb
@@ -1,18 +1,23 @@
+# class handles roulette logic
 class Wheel
 
+  # function generates random number and determines if it is red, black, or green
   def spin
     @spin = rand(37)
 
-    if (@spin >= 1) and (@spin <= 10) and (@spin.even?)
+    if @spin == 0 or @spin == 37
+
+      if @spin == 37
+        @spin == 00
+      end
+
+      puts 'Sorry, you landed on green (slot ' + @spin.to_s + '). The game is now over.'
+      exit(1)
+
+    elsif ((@spin >= 1) and (@spin <= 10) or (@spin >= 19) and (@spin <= 28)) and (@spin.even?)
       @slot = 'black'
 
-    elsif (@spin >= 11) and (@spin <= 17) and (@spin.odd?)
-      @slot = 'black'
-
-    elsif (@spin >= 20) and (@spin <= 28) and (@spin.even?)
-      @slot = 'black'
-
-    elsif (@spin >= 29) and (@spin <= 35) and (@spin.odd?)
+    elsif ((@spin >= 11) and (@spin <= 18) or (@spin >= 29) and (@spin <= 36)) and (@spin.odd?)
       @slot = 'black'
 
     else
@@ -20,16 +25,17 @@ class Wheel
     end
   end
 
+  # function calls spin function and compares spin result with bet
   def betting
     if @bank > 0 and @bank < @payout
       spin
       if @color == @slot[0,1].upcase
-        @bank = @bank + (@bet * 2)
-        puts 'You landed on ' + @slot + '.'
+        @bank += @bet
+        puts 'You landed on ' + @slot + ' (slot ' + @spin.to_s + ').'
         puts 'You won the bet! Your bank is now at $' + @bank.to_s + '.'
 
         if @bank >= @payout
-          puts 'You have reached your desire payout. The game is now over. Congratulations!'
+          puts 'You have reached/exceeded your desired payout. The game is now over. Congratulations!'
           exit(1)
         else
           Prompt.prompts(@bank)
@@ -37,8 +43,19 @@ class Wheel
 
       else
         @bank -= @bet
-        puts 'You landed on ' + @slot + '.'
+        puts 'You landed on ' + @slot + ' (slot ' + @spin.to_s + ').'
         puts 'You lost the bet. Your bank is now at $' + @bank.to_s + '.'
+
+        if @bank > 0
+          puts 'Would you like to double up?'
+          response = gets.chomp.upcase.to_s
+
+          if response.eql? 'Y'
+            double_up
+          elsif response.eql? 'N'
+            Prompt.prompts(@bank)
+          end
+        end
 
         if @bank > 0
           Prompt.prompts(@bank)
@@ -50,10 +67,19 @@ class Wheel
     end
   end
 
+  # function takes previous bet and doubles it
   def double_up
-    @number = @number
+
+    if (@bet * 2) < @bank
+      @bet *= 2
+      betting
+    else
+      puts 'You do not have enough in the bank to double up.'
+      Prompt.prompts(@bank)
+    end
   end
 
+  # constructor initializes variables for wheel class
   def initialize(bank, bet, color, payout)
     @bank = bank
     @bet = bet
@@ -62,14 +88,17 @@ class Wheel
   end
 end
 
+# class prompts user
 class Prompt
 
+  # function receives bet
   def self.place_bet
     puts 'What is your bet?'
     @bet = gets.chomp.to_i
     check_bet
   end
 
+  # Function ensures bet does not exceed bank
   def self.check_bet
     if @bet > @bank
       puts 'You cannot bet more than you have in the bank.'
@@ -77,12 +106,14 @@ class Prompt
     end
   end
 
+  # function receives color
   def self.pick_color
     puts 'Pick a color: Type "R" for red or "B" for black.'
     @color = gets.chomp.upcase.to_s
     check_color
   end
 
+  # function ensures valid color input
   def self.check_color
     unless @color.eql? 'R' or @color.eql? 'B'
       puts @color + ' is not a valid input.'
@@ -90,6 +121,7 @@ class Prompt
     end
   end
 
+  # function calls place_bet and pick_color functions and passes values to wheel class
   def self.prompts(bank)
     @bank = bank
     place_bet
@@ -98,6 +130,7 @@ class Prompt
     play.betting
   end
 
+  # program begins here
   puts 'How much money do you have?'
   @bank = gets.chomp.to_i
   puts 'What is your desired bank total?'
